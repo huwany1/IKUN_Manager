2593e90ad0dfa98d3e42f015fe9d685b0f6f8659
chyinan <chyinan2015@gmail.com>
2024-12-26 23:27:37 +0800

    log界面的歌词字符串

--- src/views/log/log.vue
+++ src/views/log/log.vue
@@ -4,7 +4,7 @@
         <el-result
           icon="success"
           title="唱"
-          sub-title="Please follow the instructions"
+          sub-title="迎面走来的你让我如此蠢蠢欲动"
         >
           <template #extra>
             <el-button type="primary">Back</el-button>
@@ -15,7 +15,7 @@
         <el-result
           icon="warning"
           title="跳"
-          sub-title="Please follow the instructions"
+          sub-title="这种感觉我从未有如此的CRASH ON YOU"
         >
           <template #extra>
             <el-button type="primary">Back</el-button>
@@ -26,7 +26,7 @@
         <el-result
           icon="error"
           title="RAP"
-          sub-title="Please follow the instructions"
+          sub-title="你是我的，我是你的谁"
         >
           <template #extra>
             <el-button type="primary">Back</el-button>
@@ -36,7 +36,7 @@
       <el-col :sm="12" :lg="6">
         <el-result icon="info" title="篮球">
           <template #sub-title>
-            <p>Using slot as subtitle</p>
+            <p>鸡你太美</p>
           </template>
           <template #extra>
             <el-button type="primary">Back</el-button>
